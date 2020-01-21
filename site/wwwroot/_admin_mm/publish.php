abd2d5d6b2e52f72b62f33e396877362ede02363
Milder Lisondra <milder.lisondra@jewsforjesus.org>
2020-01-20 19:53:17 -0800

    Modified to allow for publishing group  - pdf

--- site/wwwroot/_admin_mm/publish.php
+++ site/wwwroot/_admin_mm/publish.php
@@ -11,6 +11,8 @@ include('includes/header.php');
         <div class="page-contents">
 		
 		<input type="button" name="optoskand" id="optoskand" class="publish_button" value="Optoskand">
+		
+		<input type="button" name="PDF" id="PDF" class="publish_group" value="PDF">
 	
 	</div>
 </div>
@@ -19,10 +21,16 @@ include('includes/header.php');
 		
 		$( ".publish_button" ).on("click", function(){
 			console.log($(this).attr("id"));
-			$.post(controller,{action:"publish"});
+			product_family = $(this).attr("id");
+			$.post(controller,{action:"publish",product_family:product_family});
 		});
 		
-	  
+		var group_controller = "../_admin_mm/controllers/group.php";
+		$( ".publish_group" ).on("click", function(){
+			console.log($(this).attr("id"));
+			product_group = $(this).attr("id");
+			$.post(group_controller,{action:"publish",product_group:product_group});
+		});	  
 	</script>
 
 <?php
