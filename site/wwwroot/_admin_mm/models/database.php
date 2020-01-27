0a0cd1e50fc894624139b6feddf9bae2f71ce1c6
Milder Lisondra <milder.lisondra@jewsforjesus.org>
2020-01-27 08:50:03 -0800

    commented out Line 61

--- site/wwwroot/_admin_mm/models/database.php
+++ site/wwwroot/_admin_mm/models/database.php
@@ -43,23 +43,24 @@ class Database{
 	* @param $data array 
 	*/
 	protected function log_admin_action( $data ){
-		    $stmt = $this->conn->prepare("INSERT INTO `".$this->admin_actions."` (Admin, Action, Object,Previous_Data, Updated_Data, Data_Blob) VALUES (:Admin, :Action, :Object,:Previous_Data, :Updated_Data, :Data_Blob)");
-			$stmt->bindParam(':Admin', $user);
-			$stmt->bindParam(':Action',$action);
-			$stmt->bindParam(':Object',$object);
-			$stmt->bindParam(':Previous_Data',$previous_data, PDO::PARAM_STR);
-			$stmt->bindParam(':Updated_Data', $updated_data, PDO::PARAM_STR);
-			$stmt->bindParam(':Data_Blob', $data_blob, PDO::PARAM_STR);
-			
-			// set values
-			$user = $data['user'];
-			$action = $data['action'];
-			$object = $data['object'];
-			$previous_data = $data['previous_data'];
-			$updated_data = $data['updated_data'];
-			$data_blob = $data['data_blob'];
-			
-			$stmt->execute();			
+		 //$stmt = $this->conn->prepare("INSERT INTO `".$this->admin_actions."` (Admin, Action, Object,Previous_Data, Updated_Data, Data_Blob) VALUES (:Admin, :Action, :Object,:Previous_Data, :Updated_Data, :Data_Blob)");
+		$stmt = $this->conn->prepare("INSERT INTO `".$this->admin_actions."` (Admin, Action, Object,Previous_Data, Updated_Data) VALUES (:Admin, :Action, :Object,:Previous_Data, :Updated_Data)");
+		$stmt->bindParam(':Admin', $user);
+		$stmt->bindParam(':Action',$action);
+		$stmt->bindParam(':Object',$object);
+		$stmt->bindParam(':Previous_Data',$previous_data, PDO::PARAM_STR);
+		$stmt->bindParam(':Updated_Data', $updated_data, PDO::PARAM_STR);
+		//$stmt->bindParam(':Data_Blob', $data_blob, PDO::PARAM_STR);
+		
+		// set values
+		$user = $data['user'];
+		$action = $data['action'];
+		$object = $data['object'];
+		$previous_data = $data['previous_data'];
+		$updated_data = $data['updated_data'];
+		//$data_blob = $data['data_blob'];
+		
+		$stmt->execute();			
 	}	
 	
 	protected function log_dl( $data ){
